9f6a0a47ddb954c177847e10d04ca7839dde999e
Rayhan Faluda <rayhanfaluda@gmail.com>
2022-06-29 09:59:45 +0700

    Initial Commit

---
+++ Marvally/MarvallyApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarvallyApp.swift
+//  Marvally
+//
+//  Created by MacBook on 29/06/22.
+//
+
+import SwiftUI
+
+@main
+struct MarvallyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
